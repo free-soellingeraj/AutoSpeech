36b70cc51fe2b298aee277e022f0b7af8b49f3a3
Aaron Soellinger <asoellin@gmail.com>
2020-09-03 13:28:37 +0000

    changes sources object to dictionary; added sources as requirement to init

--- data_objects/speaker.py
+++ data_objects/speaker.py
@@ -4,22 +4,33 @@ from pathlib import Path
 
 # Contains the set of utterances of a single speaker
 class Speaker:
-    def __init__(self, root: Path, partition=None):
+    def __init__(self, sources):
         self.root = root
-        self.partition = partition
+        # self.partition = partition
         self.name = root.name
         self.utterances = None
         self.utterance_cycler = None
-        if self.partition is None:
-            with self.root.joinpath("_sources.txt").open("r") as sources_file:
-                sources = [l.strip().split(",") for l in sources_file]
-        else:
-            with self.root.joinpath("_sources_{}.txt".format(self.partition)).open("r") as sources_file:
-                sources = [l.strip().split(",") for l in sources_file]
-        self.sources = [[self.root, frames_fname, self.name, wav_path] for frames_fname, wav_path in sources]
+        self.sources = sources
+        # if self.partition is None:
+        #     p = self.root.joinpath("_sources.txt")
+        # else:
+        #     p = self.root.joinpath("_sources_{}.txt".format(self.partition))
+        # 
+        # with open(p, "r") as sources_file:
+        #     sources = [
+        #         l.strip().split(",") for l in sources_file
+        #     ]
+        # self.sources = [
+        #     [self.root, frames_fname, self.name, wav_path] 
+        #     for frames_fname, wav_path 
+        #     in sources
+        # ]
 
     def _load_utterances(self):
-        self.utterances = [Utterance(source[0].joinpath(source[1])) for source in self.sources]
+        self.utterances = [
+            Utterance(source['root'].joinpath(source['frames_fname'])) 
+            for source in self.sources
+        ]
 
     def random_partial(self, count, n_frames):
         """
